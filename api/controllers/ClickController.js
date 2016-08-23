eb1e378a1bc17cbfd61750b99b1f29cd58224de4
Fabio <islerfab@gmail.com>
2016-08-23 17:15:06 +0200

    also change the eu costs of AWS

--- api/controllers/ClickController.js
+++ api/controllers/ClickController.js
@@ -9,6 +9,8 @@ var _ = require('lodash');
 
 var cost_amazonec2_us_tier1 = '0.02';
 var cost_amazonec2_us_tier2 = '0.5';
+var cost_amazonec2_eu_tier1 = '0.02';
+var cost_amazonec2_eu_tier2 = '0.5';
 
 var HIGH = false;
 
@@ -23,10 +25,10 @@ var costs = '# HELP cost_amazonec2_us_tier1 Hourly cost for a AWS, US and tier 1
   'cost_amazonec2_us_tier3 1.0\n' +
   '# HELP cost_amazonec2_eu_tier1 Hourly cost for a AWS, EU and tier 1 machine.\n' +
   '# TYPE cost_amazonec2_eu_tier1 gauge\n' +
-  'cost_amazonec2_eu_tier1 0.02\n' +
+  'cost_amazonec2_eu_tier1 %COST3%\n' +
   '# HELP cost_amazonec2_eu_tier2 Hourly cost for a AWS, EU and tier 2 machine.\n' +
   '# TYPE cost_amazonec2_eu_tier2 gauge\n' +
-  'cost_amazonec2_eu_tier2 0.5\n' +
+  'cost_amazonec2_eu_tier2 %COST4%\n' +
   '# HELP cost_amazonec2_eu_tier3 Hourly cost for a AWS, EU and tier 3 machine.\n' +
   '# TYPE cost_amazonec2_eu_tier3 gauge\n' +
   'cost_amazonec2_eu_tier3 1.0\n' +
@@ -62,7 +64,10 @@ module.exports = {
         var counts = _.map(grouped, function (value, key) {
           return {origin: key, count: value.length * multiplier};
         });
-        var result = costs.replace('%COST1%', cost_amazonec2_us_tier1).replace('%COST2%', cost_amazonec2_us_tier2);
+        var result = costs.replace('%COST1%', cost_amazonec2_us_tier1)
+                          .replace('%COST2%', cost_amazonec2_us_tier2)
+                          .replace('%COST3%', cost_amazonec2_eu_tier1)
+                          .replace('%COST4%', cost_amazonec2_eu_tier2);
         _.forEach(counts, function (count) {
           result += '# HELP click_count_' + count.origin + ' The total number of clicks from ' + count.origin + '.\n# TYPE click_count_' + count.origin + ' counter\n';
           result += 'click_count_' + count.origin + ' ' + count.count + '\n';
@@ -93,6 +98,8 @@ module.exports = {
   setHighCosts: function (req, res) {
     cost_amazonec2_us_tier1 = '1.0';
     cost_amazonec2_us_tier2 = '1.0';
+    cost_amazonec2_eu_tier1 = '1.0';
+    cost_amazonec2_eu_tier2 = '1.0';
     HIGH = true;
     res.ok();
   },
@@ -100,6 +107,8 @@ module.exports = {
   setLowCosts: function (req, res) {
     cost_amazonec2_us_tier1 = '0.02';
     cost_amazonec2_us_tier2 = '0.5';
+    cost_amazonec2_eu_tier1 = '0.02';
+    cost_amazonec2_eu_tier2 = '0.5';
     HIGH = false;
     res.ok();
   }
